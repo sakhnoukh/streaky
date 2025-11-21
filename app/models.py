b574f8bb1fc1b02133966304cd4fa7732ba6df0d
felix <felix.lwestin@gmail.com>
2025-11-21 16:49:42 +0100

    revert: back to working version without registration

--- app/models.py
+++ app/models.py
@@ -10,7 +10,6 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
 
 class Habit(Base):
     __tablename__ = "habits"
